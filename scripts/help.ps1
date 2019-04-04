a6dc68d36efd9e08913c0de2a2d24a71c8272d1e
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-04-03 19:00:25 -0500

    Updated the Help section to be project independent.
    Revised the Help Section to be independent to the project itself - instead of relying on another project's information.  The reason for this change is necessary to standout as its own; all information should be generalized to itself - not reflecting towards another project.

--- scripts/help.ps1
+++ scripts/help.ps1
@@ -1,18 +1,32 @@
 ﻿<#
 .SYNOPSIS
-    Compiles the Alphecca project in ZDoom's PK[3|7] standards.
+    Compiles a ZDoom based mod project to a PK3 or PK7 archive datafile.
 
 .DESCRIPTION
-    This program is a compiler for the Alphecca project.  Because the project from the development source is incompatible with ZDoom's ZIP Standards, this program is designed to transform the project hierarchy to the specific standards set from ZDoom.  In addition, this program will allow the user flexibility with how the project is to be compacted.  As ZDoom allows some flexibility in terms of compression levels and algorithms, this program will also provide the same adjustability.  After a build has been successfully compiled, the user can then merely play the newly generated build with the desired Doom Engine, such as GZDoom.
-    NOTE: This program will require .NET 5.0 minimum, 7Zip for PK7 support, and GIT for dev. builds.  Please see the project's wiki for more information.
+    This program provides the ability to compile a ZDoom family based project source into either a PK3 or PK7 archive datafile.  With transforming the project's source to an archive datafile to either PK3 or PK7: this allows any modern ZDoom family engine to easily read the file, exporting the archive file to any webservices, sharing with user's abroad on the internet, and to neatly organize different builds for development purposes.
 
+    Make sure that your project source (or custom source) follows the ZDoom's ZIP Specifications.  For more information, please look at the following resources:
+        https://zdoom.org/wiki/Using_ZIPs_as_WAD_replacement
+
+    Tools that this program utilizes are:
+        dotNET Framework 5.0 Minimum (Windows Platform)
+            The required framework that is needed in order for this program to run successfully on the Windows Platform.
+        dotNET Core 2.1.x Minimum (*nix Platform)
+            The required framework that is needed in order for this program to run successfully on the *nix Platform.
+        7Zip [Optional]
+            Compact the project source to either a PK3 or PK7 archive datafile.
+        Git [Optional]
+            Ideal for Alpha and Beta Development Builds.
+            Retrieve project's repository information and pull updates to project's local repository.
+        Microsoft Word 2016 or Later [Optional]
+            Generate a report in Portable Document File (PDF).
 .NOTES
     Author: Nicholas Gautier
     Email: Nicholas.Gautier.Tiger@GMail.com
-    Project Website: https://github.com/SibTiger/Alphecca
+    Project Website: https://github.com/SibTiger/PowerShell-Compact-Archive-Tool
 
 .INPUTS
-    Nothing is to be given or to be provided from a command\pipe.
+    Nothing is to be given or to be provided from a command or pipe.
 
 .OUTPUTS
     Nothing is to be returned or to be sent to the pipe.
@@ -21,6 +35,6 @@
     .\Compile.ps1
 
 .LINK
-    https://github.com/SibTiger/Alphecca
+    https://github.com/SibTiger/PowerShell-Compact-Archive-Tool
     https://zdoom.org/wiki/Using_ZIPs_as_WAD_replacement
 #>
