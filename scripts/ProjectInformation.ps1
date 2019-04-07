8630a54e3d61ff48b3e6cde7da9359d3897f2848
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-04-07 01:29:18 -0500

    ProjectInformation is now entirely static
    Because this class is not dynamic in anyway and only exists to provide the project's information (which is never changed), it is best to change this function as a static.  However, because it is now static, this class contains further changes.  This class no longer contains any Getter functions to retrieve the variable data, also the variables within this class are no longer considered 'hidden'.  Thus, the variables are now the only way to view the data directly.

--- scripts/ProjectInformation.ps1
+++ scripts/ProjectInformation.ps1
@@ -19,25 +19,24 @@ class ProjectInformation
     # =================================================
 
 
-    #region Private Variables (emulated)
-
     # Project's Name
     # ---------------
     # The formal name of the project.
-    Hidden [string] $__projectName;
+    Static [string] $projectName = "Alphecca";
 
 
     # Project's Code Name (or Version Code Name)
     # ---------------
     # The code name of the project or of that version.
-    Hidden [string] $__codeName;
+    Static [string] $codeName = "Apostasy";
 
 
     # Output Filename
     # ---------------
     # The filename of the archive data file in which this program
-    # will create as requested by the end-user.
-    Hidden [string] $__fileName;
+    #  will create as requested by the end-user.
+    # NOTE: Remember that /idgames has an 8char limit
+    Static [string] $fileName = "alphecca";
 
 
     # Project's Website
@@ -46,7 +45,7 @@ class ProjectInformation
     # end-user by request.  This can be helpful to the user, in-which
     # they may check out the project's latest public announcements
     # and insights regarding the project.
-    Hidden [string] $__urlWebsite;
+    Static [string] $urlWebsite = "https://github.com/SibTiger/Alphecca";
 
 
     # Project's Help Documentation
@@ -57,7 +56,7 @@ class ProjectInformation
     # reasons but not limited to using this very tool.
     #  NOTE: Wiki's are usually provided in some Developer\Repositories
     #  Web-Services, such as GitHub.
-    Hidden [string] $__urlWiki;
+    Static [string] $urlWiki = "https://github.com/SibTiger/Alphecca/wiki";
 
 
     # Project's Repository
@@ -67,147 +66,5 @@ class ProjectInformation
     # source code via their preferred web-browser.
     # Prime example of Repositories:
     #  GitHub, SourceForge, BitBucket, GitLab, and many more.
-    Hidden [string] $__urlSource;
-    
-    #endregion
-
-
-
-    # Member Functions :: Methods
-    # =================================================
-    # =================================================
-
-
-    #region Constructor Functions
-    
-    # Default Constructor
-    ProjectInformation()
-    {
-        # Project name
-        $this.__projectName = "Alphecca";
-
-        # Code name (Version name)
-        $this.__codeName = "Apostasy";
-
-        # File name (Archive datafile name)
-        #  NOTE: Remember that /idgames has an 8char limit
-        $this.__fileName = "alphecca";
-
-        # Project's Website URL
-        $this.__urlWebsite = "https://github.com/SibTiger/Alphecca";
-
-        # Project's Wiki URL
-        $this.__urlWiki = "https://github.com/SibTiger/Alphecca/wiki";
-
-        # Project's Source (Repository) URL
-        $this.__urlSource = "https://github.com/SibTiger/Alphecca";
-    } # Default Constructor
-
-    #endregion
-
-
-
-    #region Getter Functions
-
-   <# Get Project Name
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Project Name variable.
-    # -------------------------------
-    # Output:
-    #  [string] Project Name
-    #   The name of the project.
-    # -------------------------------
-    #>
-    [string] GetProjectName()
-    {
-        return $this.__projectName;
-    } # GetProjectName()
-
-
-
-   <# Get Project's Code Name
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Code Name variable.
-    # -------------------------------
-    # Output:
-    #  [string] Project's Code Name
-    #   The code name of the project or version.
-    # -------------------------------
-    #>
-    [string] GetCodeName()
-    {
-        return $this.__codeName;
-    } # GetCodeName()
-
-
-
-   <# Get Filename
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the filename variable.
-    # -------------------------------
-    # Output:
-    #  [string] Archive datafile filename
-    #   The filename to be used for the archive datafile.
-    # -------------------------------
-    #>
-    [string] GetFilename()
-    {
-        return $this.__fileName;
-    } # GetFilename()
-
-
-
-   <# Get Project's Official Website
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Website variable.
-    # -------------------------------
-    # Output:
-    #  [string] Project's Website
-    #   The project's official website.
-    # -------------------------------
-    #>
-    [string] GetProjectWebsite()
-    {
-        return $this.__urlWebsite;
-    } # GetProjectWebsite()
-
-
-
-   <# Get Project's Official Documentation
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Wiki\Documentation variable.
-    # -------------------------------
-    # Output:
-    #  [string] Project's Wikipedia
-    #   The project's official wikipedia\documentation webpage.
-    # -------------------------------
-    #>
-    [string] GetProjectWiki()
-    {
-        return $this.__urlWiki;
-    } # GetProjectWiki()
-
-
-
-   <# Get Project's Official Repository
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Source variable.
-    # -------------------------------
-    # Output:
-    #  [string] Project's Repository
-    #   The project's official repository (web view) path.
-    # -------------------------------
-    #>
-    [string] GetProjectSource()
-    {
-        return $this.__urlSource;
-    } # GetProjectSource()
-
-    #endregion
+    Static [string] $urlSource = "https://github.com/SibTiger/Alphecca";
 } # ProjectInformation
