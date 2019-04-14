4ad2ba6eb058d90053fcb13aa2fcadcccaf11cb8
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-04-14 16:26:39 -0500

    Spell check

--- scripts/Initializations.ps1
+++ scripts/Initializations.ps1
@@ -31,7 +31,7 @@ function InitalizationProgramData()
     Set-Variable -Name "_PROGRAMNAME_" -Value "PowerShell Compact-Archive Tool" `
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
-    # Program Name (Abbrivated)
+    # Program Name (Abbreviated)
     Set-Variable -Name "_PROGRAMNAMESHORT_" -Value "PSCAT" `
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
