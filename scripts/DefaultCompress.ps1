e87461c12294a46f9621a774b91979001eb84a19
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-09-25 19:18:51 -0500

    Resolved a bug the logging logic in CreateArchive()
    Resolved a possible bug when evaluating the logging state within the program. If the DebugLoggingState is false and the CreateDirectories were to fail, the function would return an error - despite that the logging state is false.

--- scripts/DefaultCompress.ps1
+++ scripts/DefaultCompress.ps1
@@ -2174,8 +2174,8 @@ class DefaultCompress
         #   This check is to make sure that nothing goes horribly wrong.
         # ---------------------------
 
-        # Make sure that the .NET Compress Archive Logging directories are ready for use (if required)
-        if ([Logging]::DebugLoggingState() -and ($this.__CreateDirectories() -eq $false))
+        # Make sure that the logging requirements are met.
+        if (([Logging]::DebugLoggingState() -eq $true) -and ($this.__CreateDirectories() -eq $false))
         {
             # Because the logging directories could not be created, we can not log.
             #  Because the logging features are required, we can not run the operation.
