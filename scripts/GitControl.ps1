b0d357c0795d6dd2df36327a61c8a094df9f2dee
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2020-06-25 20:16:51 -0500

    Forgot to remove testing variable
    I completely forgot to remove the testing variable that I made yesterday to help test the negative conditional statement.

--- scripts/GitControl.ps1
+++ scripts/GitControl.ps1
@@ -2045,7 +2045,6 @@ class GitControl
         # ++++++++++++++++++++
         # How many commits are to be fetched from the project's local repository?
 
-        $this.__changelogLimit = -50;
         # Retrieve all of the commits that had ever been made to the Project's local repository?
         if ($this.__changelogLimit -eq 0)
         {
