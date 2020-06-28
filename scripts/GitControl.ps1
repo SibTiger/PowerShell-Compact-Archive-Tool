ec4281936e19fe1e156b6df55daa24cb81f0998c
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2020-06-27 22:55:12 -0500

    Revised and provided documentation

--- scripts/GitControl.ps1
+++ scripts/GitControl.ps1
@@ -2384,43 +2384,44 @@ class GitControl
         # Make sure that the Git Logging directories are ready for use (if required)
         if ([Logging]::DebugLoggingState() -and ($this.__CreateDirectories() -eq $false))
         {
-            # Because the logging directories could not be created, we can not log.
-            #  Because the logging features are required, we can not run the operation.
             return $false;
+            # Because the logging directories could not be created, we cannot log.
+            # Because the logging features are required, we cannot run the operation.
         } # If : Git Logging Directories
 
 
-        # Make sure that the git executable was detected.
+        # Make sure that the Git executable has been detected and is presently ready to be used.
         if ($($this.DetectGitExist()) -eq $false)
         {
-            # Git was not detected, throw the error message instead.
             return "ERR";
+            # The Git executable was not detected.
+            # Because the Git application was not found, return an error to signify that the operation had failed.
         } # if : Git was not detected
 
 
-        # Make sure that the path exists
+        # Make sure that the Project Directory exists within the provided path.
         if ($([IOCommon]::CheckPathExists("$($projectPath)", $true)) -eq $false)
         {
-            # Project Path does not exist, return the default
-            #  error message instead.
             return "ERR";
-        } # if : the Project Path does not exist
+            # The project directory does not exist with the provided path, unable to proceed forward.
+            # Return a failure to signal that the operation had failed.
+        } # if : The Project Directory does not exist
 
         # ---------------------------
         # - - - - - - - - - - - - - -
 
 
         # Execute the command
-        [IOCommon]::ExecuteCommand("$($this.__executablePath)", `
-                            "$($extCMDArgs)", `
-                            "$($projectPath)", `
-                            "$($this.__logPath)", `
-                            "$($this.__logPath)", `
-                            "$($this.__reportPath)", `
-                            "$($execReason)", `
-                            $false, `
-                            $true, `
-                            [ref]$outputResult) | Out-Null;
+        [IOCommon]::ExecuteCommand("$($this.__executablePath)", `       # Git Executable Path
+                                    "$($extCMDArgs)", `                 # Arguments to retrieve all of the available branches within the local repo.
+                                    "$($projectPath)", `                # The working directory that Git will start from.
+                                    "$($this.__logPath)", `             # The Standard Output Directory Path.
+                                    "$($this.__logPath)", `             # The Error Output Directory Path.
+                                    "$($this.__reportPath)", `          # The Report Directory Path.
+                                    "$($execReason)", `                 # The reason why we are running Git; used for logging purposes.
+                                    $false, `                           # Are we building a report?
+                                    $true, `                            # Do we need to capture the STDOUT so we can process it further?
+                                    [ref]$outputResult) | Out-Null;     # Variable containing the STDOUT; if we need to process it.
 
 
         # Just for assurance; make sure that we have all of the branches.
