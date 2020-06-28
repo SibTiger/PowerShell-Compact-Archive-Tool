c250d983d678e44ffe9cec746e5e59ac9654535e
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2020-06-28 12:17:25 -0500

    Revised the documentation

--- scripts/GitControl.ps1
+++ scripts/GitControl.ps1
@@ -2785,26 +2785,28 @@ class GitControl
         # Make sure that the Git Logging directories are ready for use (if required)
         if (([Logging]::DebugLoggingState() -eq $true) -and ($this.__CreateDirectories() -eq $false))
         {
-            # Because the logging directories could not be created, we can not log.
-            #  Because the logging features are required, we can not run the operation.
             return $false;
+            # Because the logging directories could not be created, we cannot log.
+            # Because the logging features are required, we cannot run the operation.
         } # If : Git Logging Directories
 
 
-        # Make sure that the git executable was detected.
+        # Make sure that the Git executable has been detected and is presently ready to be used.
         if ($($this.DetectGitExist()) -eq $false)
         {
-            # Git was not detected, return an error message instead.
             return "ERR";
+            # The Git executable was not detected.
+            # Because the Git application was not found, return an error to signify that the operation had failed.
         } # if : Git was not detected
 
 
-        # Make sure that the path exists
+        # Make sure that the Project Directory exists within the provided path.
         if ($([IOCommon]::CheckPathExists("$($projectPath)", $true)) -eq $false)
         {
-            # Project Path does not exist, return an error message instead.
             return "ERR";
-        } # if : the Project Path does not exist
+            # The project directory does not exist with the provided path, unable to proceed forward.
+            # Return a failure to signal that the operation had failed.
+        } # if : The Project Directory does not exist
 
         # ---------------------------
         # - - - - - - - - - - - - - -
