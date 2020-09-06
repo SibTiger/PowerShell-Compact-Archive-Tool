120466fd6b12e575f10e4b661bb5244ca881fbf9
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2020-09-05 20:34:40 -0500

    Minor wording corrections.

--- scripts/help.ps1
+++ scripts/help.ps1
@@ -1,18 +1,18 @@
 ﻿<#
 .SYNOPSIS
-    Compiles a ZDoom based mod project to a PK3 or PK7 archive datafile.
+    Compiles a ZDoom based mod project in to an PK3 or PK7 archive datafile.
 
 .DESCRIPTION
-    This program provides the ability to compile a ZDoom family based project source into either a PK3 or PK7 archive datafile.  With transforming the project's source to an archive datafile to either PK3 or PK7: this allows any modern ZDoom family engine to easily read the file, exporting the archive file to any webservices, sharing with user's abroad on the internet, and to neatly organize different builds for development purposes.
+    This program provides the ability to compile a ZDoom family-based project source into either an PK3 or PK7 archive datafile.  With transforming the project's source to an archive datafile to either PK3 or PK7: this allows any modern ZDoom family engine to easily read the file, exporting the archive file to any webservices, sharing with user's abroad on the internet, and to neatly organize different builds for development purposes.
 
     Make sure that your project source (or custom source) follows the ZDoom's ZIP Specifications.  For more information, please look at the following resources:
         https://zdoom.org/wiki/Using_ZIPs_as_WAD_replacement
 
     Tools that this program utilizes are:
         dotNET Framework 5.0 Minimum (Windows Platform)
-            The required framework that is needed in order for this program to run successfully on the Windows Platform.
+            The required framework that is needed for this program to run successfully on the Windows Platform.
         dotNET Core 2.1.x Minimum (*nix Platform)
-            The required framework that is needed in order for this program to run successfully on the *nix Platform.
+            The required framework that is needed for this program to run successfully on the *nix Platform.
         7Zip [Optional]
             Compact the project source to either a PK3 or PK7 archive datafile.
         Git [Optional]
