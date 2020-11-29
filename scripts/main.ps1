65f9017041af12f44d37f795f43f9f7ff2e44f60
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2020-11-29 12:56:17 -0600

    Provided documentation
    Provided documentation for the spine of the application.

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,13 +1,31 @@
-﻿# Program Entry Point
+﻿<# Main Entry Point
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This source file will initialize the application's environment, assuring that the program can run within the host's
+ #  PowerShell instance, provide a means for the user to interact within the software, and then - once the user had finished
+ #  using the program - termination by safely closing the environment that had been created.
+ #
+ # Essentially, this is the spine of the entire program.  If the entry point fails, the entire application will fall apart.
+ #>
+
+
+
+
+# Program Entry Point
 # --------------------------
 # Documentation:
-#     This is the main spine of the program.
+#   This function is our entry point for the program.  This function will be in charge of setting up the environment,
+#   assuring host compatibility, executing the Main Menu (User Interaction), and then self termination once the user
+#   leaves the Main Menu.
 # --------------------------
 function main()
 {
-
+s
 } # main()
 
 
 
+
+# Execute the application
 main;
