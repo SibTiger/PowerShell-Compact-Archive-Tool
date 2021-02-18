78ea1dba3fb7133e5ecc4aa1ff25654b8195b5bc
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-02-17 21:32:50 -0600

    Created a new empty class: CommonCUI
    The Common CUI class will hold all of the text and formatting that are essentially common through out the entire program.  This will help to reduce code duplication, but yet - provide consistency for the program's UI.

---
+++ scripts/CommonCUI.ps1
@@ -0,0 +1,29 @@
+<# Common CUI
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This class will hold common Console User Interface instructions and procedures to
+ #  reduce code duplication.  With the functions provided in this class, we can easily
+ #  keep most of the User Interface content unified and consistent through out the
+ #  entire program.  With the consistency of the UI, the end-user can easily navigate
+ #  within the program with the outmost ease.  If we were not consistent, the user
+ #  will easily get frustrated with the program.
+ #>
+
+
+
+
+class CommonCUI
+{
+   <# Main Menu Driver
+    # -------------------------------
+    # Documentation:
+    #   This function will output the program's title header onto the Terminal's
+    #   buffer (screen).
+    # -------------------------------
+    #>
+    static [void] ProgramTitleHeader()
+    {
+
+    } # ProgramTitleHeader()
+} # CommonCUI
