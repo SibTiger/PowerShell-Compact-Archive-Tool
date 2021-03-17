410e1d7681d023dd445c78bd7bfc58821469877a
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-03-16 20:16:54 -0500

    Added a new class, Settings - - VERY WIP!!!
    Finally came to a near conclusion on the filename and the description.  I was not able to make up my mind yesterday on this.
    
    I am still building this class, but I have something to work with now.

---
+++ scripts/Settings.ps1
@@ -0,0 +1,25 @@
+<# Settings
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This class will allow the user to easily modify their settings
+ #  from various known objects, in which contains variables that
+ #  are configurable within the designated object - and friendly
+ #  within the program's environment.  Because the idea of the
+ #  program's objects are not supposed to integrate its settings
+ #  per-class, we will need to use a centralized settings class
+ #  to unify the User Interface and functionality, while allowing
+ #  all of the objects to be configured respectively.  This is
+ #  where this class comes into play.  We will want to be sure
+ #  that we can easily allow the user to configure their settings
+ #  and to make it as painless as possible.
+ #>
+
+
+
+
+
+class Settings
+{
+
+} # Settings
