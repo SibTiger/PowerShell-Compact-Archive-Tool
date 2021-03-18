8e3cedd690323680a4b659f20d072fe43dc973eb
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-03-17 19:30:28 -0500

    [WIP] Added a new function: Main and DrawMainSettingsMenu
    - Added a new function called Main(), which will provide the main settings menu to the user.
    - Added a new function called DrawMainSettingsMenu().  This will draw the menu items of the objects that can be configured to the user.
    
    These functions are not quite ready to be used.

--- scripts/Settings.ps1
+++ scripts/Settings.ps1
@@ -21,5 +21,80 @@
 
 class Settings
 {
+   <# Main Settings Menu Driver
+    # -------------------------------
+    # Documentation:
+    #  This function allows the ability for the user to select which object
+    #   to modify.  The objects provided are part of the Singleton methodology
+    #   and can be easily manipulated, if the getters\setters are properly
+    #   provided for the desired member variables.
+    # -------------------------------
+    #>
+    static [void] Main()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This variable will hold the user's input as they navigate within the main settings menu.
+        [string] $userInput = $null;
 
+        # This variable will determine if the user is to remain within the Main Settings Menu loop.
+        #  If the user wanted to return back to the Main Menu, this variable's state will be set as
+        #  false.  Thus, with a false value - they may return back to the Main Menu.
+        [bool] $mainSettingsMenuLoop = $true;
+        # ----------------------------------------
+
+        # Open the Main Settings Menu
+        #  Keep the user at the Main Settings Menu until they request to return back to the Main Menu.
+        do
+        {
+            # Clear the terminal of all previous text; keep the space clean so that
+            #  it is easy for the user to read and follow along.
+            [CommonIO]::ClearBuffer();
+
+            # Draw Program Information Header
+            [CommonCUI]::DrawProgramTitleHeader();
+
+            # Show the user that they are at the Main Menu
+            [CommonCUI]::DrawSectionHeader("User Preference Main Menu");
+
+            # Display the instructions
+            [CommonCUI]::DrawMenuInstructions();
+
+            # Draw the Main Menu list to the user
+            [MainMenu]::DrawMainSettingsMenu();
+        } while ($mainSettingsMenuLoop)
+    } # Main()
+
+
+
+
+<# Draw Main Settings Menu
+    # -------------------------------
+    # Documentation:
+    #  This function will essentially draw the Main Settings Menu list to the user.  Thus this function
+    #   provides to the user as to what classes are available to configure.
+    # -------------------------------
+    #>
+    static [void] DrawMainSettingsMenu()
+    {
+        # Display the Main Settings Menu list
+
+        # Generate Project and View Project Information
+        [CommonCUI]::DrawMenuItem('P', "Configure General $([ProjectInformation]::projectName) Preferences");
+        [CommonCUI]::DrawMenuItem('Z', "Configure Zip Preferences");
+        [CommonCUI]::DrawMenuItem('7', "Configure 7Zip Preferences");
+        [CommonCUI]::DrawMenuItem('G', "Configure Git Preferences");
+
+
+        # Program Tools
+        [CommonCUI]::DrawMenuItem('?', "Help Documentation");
+
+
+        # Return back to the Main Menu
+        [CommonCUI]::DrawMenuItem('X', "Go back to Main Menu");
+
+
+        # Provide some extra padding
+        [Logging]::DisplayMessage("`r`n");
+    } # DrawMainSettingsMenu()
 } # Settings
