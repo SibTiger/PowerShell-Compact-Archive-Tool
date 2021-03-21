45181305aecc29a070c7a0980e746925f07cd19f
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-03-21 16:44:11 -0500

    Do not show Zip if it is not available
    - Added a new function IsAvailableZip(), which returns a boolean value if the dotNET Core Zip functionality was detected by the program.
    - Revised the Main Settings Menu to not display Zip if the feature is not available to the user.

--- scripts/Settings.ps1
+++ scripts/Settings.ps1
@@ -88,7 +88,15 @@ class Settings
 
         # Generate Project and View Project Information
         [CommonCUI]::DrawMenuItem('P', "Configure General $([ProjectInformation]::projectName) Preferences");
-        [CommonCUI]::DrawMenuItem('Z', "Configure Zip Preferences");
+
+
+
+        # Only show this option if it is available to the user
+        if ([Settings]::IsAvailableZip() -eq $true)
+        {
+            # Option is available, so display it on the settings main menu.
+            [CommonCUI]::DrawMenuItem('Z', "Configure Zip Preferences");
+        } # if: Display .NET Core ZIP Option
 
 
         # Only show this option if it is available to the user
@@ -203,4 +211,34 @@ class Settings
         # Return the results from the detection function
         return $gitControl.DetectGitExist();
     } # IsAvailableGit()
+
+
+
+
+   <# Is .NET Core ZIP Archive Available?
+    # -------------------------------
+    # Documentation:
+    #  This function will determine if the .NET Core Zip  functionality
+    #   is available on the host system.  In order for this operation
+    #   to work, we will use the Default Zip object to check if such
+    #   feature is present.
+    # -------------------------------
+    # Output:
+    #  [bool] .NET Core Zip Availability
+    #   When true, this will mean that Zip is available and can be used.
+    #   False, however, will mean that the Zip functionality is not available.
+    # -------------------------------
+    #>
+    static [bool] IsAvailableZip()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # Latch onto the single instance of the Zip object
+        [DefaultCompress] $defaultCompress = [DefaultCompress]::GetInstance();
+        # ----------------------------------------
+
+
+        # Return the results from the detection function
+        return $defaultCompress.DetectCompressModule();
+    } # IsAvailableZip()
 } # Settings
