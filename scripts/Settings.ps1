2f5d27ed5bdbd782b748bd83ec55d972770aee35
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-03-22 18:43:50 -0500

    Changing the model of the class a bit
    Instead of having the Settings Class hold EVERYTHING, we will instead just separate each object's settings into its own sub-class from Settings.  This will make the objects cleaner, and easier to read and maintain.

--- scripts/Settings.ps1
+++ scripts/Settings.ps1
@@ -2,17 +2,14 @@
  # ------------------------------
  # ==============================
  # ==============================
- # This class will allow the user to easily modify their settings
- #  from various known objects, in which contains variables that
- #  are configurable within the designated object - and friendly
- #  within the program's environment.  Because the idea of the
- #  program's objects are not supposed to integrate its settings
- #  per-class, we will need to use a centralized settings class
- #  to unify the User Interface and functionality, while allowing
- #  all of the objects to be configured respectively.  This is
- #  where this class comes into play.  We will want to be sure
- #  that we can easily allow the user to configure their settings
- #  and to make it as painless as possible.
+ # This class will allow the user to easily access their desired
+ #  settings in which they wish to configure.  To avoid
+ #  over-bloating this class, each main object setting will have
+ #  their own Settings Class.  Thus, allowing each object to
+ #  handle its intended operation - done well.  This class,
+ #  will act as a gateway into accessing other settings, if
+ #  and only if the object is available to the user in the current
+ #  instance of the program.
  #>
 
 
