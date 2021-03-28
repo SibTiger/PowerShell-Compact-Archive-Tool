df62189931153ae1f7e073a0d7addcc4aab1df58
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-03-28 16:06:42 -0500

    Keyboard is sensitive....  Removed an unintentional double period

--- scripts/CommonFunctions.ps1
+++ scripts/CommonFunctions.ps1
@@ -5,7 +5,7 @@
  # This class will hold functions that are commonly used within the program.
  #  Functions provided are in a general form, thus they can be used anywhere
  #  and everywhere - and they provide the same procedure and behavior regardless
- #  where called within the program..
+ #  where called within the program.
  #>
 
 
