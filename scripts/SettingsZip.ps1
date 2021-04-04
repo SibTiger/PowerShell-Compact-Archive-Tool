29d42dbe077acd6e1866f2cf53d500656a310f7b
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2021-04-04 16:51:24 -0500

    update SettingsZip.ps1

--- scripts/SettingsZip.ps1
+++ scripts/SettingsZip.ps1
@@ -120,8 +120,8 @@
          switch ($userRequest)
          {
              # Compression Level
-             #  NOTE: Allow the user's request when they type: 'Compress', 'Compression Level',
-             #           ' as well as '?'.
+             #  NOTE: Allow the user's request when they type: 'Compress',
+             #           'Compression Level', as well as 'C'.
              {($_ -eq "C") -or `
                  ($_ -eq "Compression Level") -or `
                  ($_ -eq "Compress")}
