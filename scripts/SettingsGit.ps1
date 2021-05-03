96a4e2c8638044c68aae50f05183856410963fb2
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-05-03 18:27:16 -0500

    It is now possible for the user to specify the Git executable path.

--- scripts/SettingsGit.ps1
+++ scripts/SettingsGit.ps1
@@ -153,6 +153,25 @@ class SettingsGit
     {
         switch ($userRequest)
         {
+            # Browse for Git
+            #  NOTE: Allow the user's request when they type: 'Browse for Git', 'Find Git',
+            #           'Locate Git', 'Browse Git', as well as 'B'.
+            {($_ -eq "B") -or `
+                ($_ -eq "Browse for Git") -or `
+                ($_ -eq "Find Git") -or `
+                ($_ -eq "Locate Git") -or `
+                ($_ -eq "Browse Git")}
+            {
+                # Allow the user to locate the path to Git or verify Git's path.
+                [SettingsGit]::LocateGitPath()
+
+
+                # Finished
+                break;
+            } # Browse for Git
+
+
+
             # Access the Help Program's Documentation
             #  NOTE: Allow the user's request when they type: 'Help', 'Helpme',
             #           'Help me', as well as '?'.
@@ -210,4 +229,266 @@ class SettingsGit
         # Finished with the operation; return back to the current menu.
         return $true;
     } # EvaluateExecuteUserRequest()
+
+
+
+
+
+    #region Locate Git Path
+    #                                     Locate Git Path
+    # ==========================================================================================
+    # ------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------
+    # ==========================================================================================
+
+
+
+
+
+   <# Locate Git's Path
+    # -------------------------------
+    # Documentation:
+    #  This function will allow the user the ability to customize the Git path, so that this
+    #   program can be able to utilize Git's functionality and features.
+    # -------------------------------
+    #>
+    hidden static [void] LocateGitPath()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This variable will hold the user's input as they navigate within the menu.
+        [string] $userInput = $null;
+
+        # This variable will determine if the user is to remain within the current menu loop.
+        #  If the user were to exit from the menu, this variable's state will be set as false.
+        #  Thus, with a false value - they may leave from the menu.
+        [bool] $menuLoop = $true;
+
+        # Retrieve the current instance of the Git object; this contains the user's settings
+        #  when using the Git application.
+        [GitControl] $gitControl = [GitControl]::GetInstance();
+        # ----------------------------------------
+
+        # Open the Locate Git Path Configuration menu
+        #  Keep the user within the menu until they request to return back to the previous menu.
+        do
+        {
+            # Clear the terminal of all previous text; keep the space clean so that it is easy
+            #  for the user to read and follow along.
+            [CommonIO]::ClearBuffer();
+
+            # Draw Program Information Header
+            [CommonCUI]::DrawProgramTitleHeader();
+
+            # Show the user that they are at the Locate Git Path menu
+            [CommonCUI]::DrawSectionHeader("Locate Git Path");
+
+            # Provide the current Git path
+            #  Determine how to present to the user that the path is valid or not.
+            if ([CommonIO]::CheckPathExists("$($gitControl.GetExecutablePath())", $true))
+            {
+                # The path was found, so provide a nice message to the user - letting them know that
+                #  the program can find the Git application.
+                [Logging]::DisplayMessage("I can find the Git application within the following path:");
+            } # if : Git Application was found
+
+            # Display that the Git path is not valid
+            else
+            {
+                # The path was not found, so provide a nice message to the user - letting them know that
+                #  the program cannot find the Git application.
+                [Logging]::DisplayMessage("I cannot find the Git application within the provided path:")
+            } # Else: Git's path was not found
+
+            # Output the project's path
+            [Logging]::DisplayMessage("`t$($gitControl.GetExecutablePath())");
+
+            # Provide some extra white spacing so that it is easier to read for the user
+            [Logging]::DisplayMessage("`r`n`r`n");
+
+            # Display the instructions to the user
+            [CommonCUI]::DrawMenuInstructions();
+
+            # Draw the menu list to the user
+            [SettingsGit]::DrawMenuLocateGitPath();
+
+            # Provide some extra padding
+            [Logging]::DisplayMessage("`r`n");
+
+            # Capture the user's feedback
+            $userInput = [CommonCUI]::GetUserInput("WaitingOnYourResponse");
+
+            # Execute the user's request
+            $menuLoop = [SettingsGit]::EvaluateExecuteUserRequestLocateGitPath($userInput);
+        } while ($menuLoop);
+    } # LocateGitPath()
+
+
+
+
+   <# Draw Menu: Locate Git Path
+    # -------------------------------
+    # Documentation:
+    #  This function will essentially draw the menu list for the Locate Git Path to the user.
+    #   thus this provides what options are available to the user in order to configure the Git
+    #   path.
+    # -------------------------------
+    #>
+    hidden static [void] DrawMenuLocateGitPath()
+    {
+        # Display the Menu List
+
+        # Change the Git Path
+        [CommonCUI]::DrawMenuItem('C', `
+                                "Change Path", `
+                                "Locate the directory that contains the Git application.", `
+                                $true);
+
+
+        # Return back to the previous menu
+        [CommonCUI]::DrawMenuItem('X', `
+                                "Cancel", `
+                                "Return back to the previous menu.", `
+                                $true);
+    } # DrawMenuLocateGitPath()
+
+
+
+
+   <# Evaluate and Execute User's Request: Locate Git Path
+    # -------------------------------
+    # Documentation:
+    #  This function will evaluate and execute the user's desired request in respect to
+    #   the menu options that were provided to them.
+    # -------------------------------
+    # Input:
+    #  [string] User's Request
+    #   This will provide the user's desired request to run an operation or to access
+    #    a specific functionality.
+    # -------------------------------
+    # Output:
+    #  [bool] User Stays at Menu
+    #   This defines if the user is to remain at the Menu screen.
+    #       $true  = User is to remain at the Menu.
+    #       $false = User requested to leave the Menu.
+    # -------------------------------
+    #>
+    hidden static [bool] EvaluateExecuteUserRequestLocateGitPath([string] $userRequest)
+    {
+        # Evaluate the user's request
+        switch ($userRequest)
+        {
+            # Change the Git Path
+            #  NOTE: Allow the user's request when they type: 'Change Path', 'Change',
+            #           'Update Path', 'Update', as well as 'C'.
+            {($_ -eq "C") -or `
+                ($_ -eq "Change Path") -or `
+                ($_ -eq "Change") -or `
+                ($_ -eq "Update Path") -or `
+                ($_ -eq "Update")}
+            {
+                # Configure the path of the Git directory.
+                [SettingsGit]::LocateGitPathNewPath();
+
+
+                # Finished
+                break;
+            } # Change Path
+
+
+            # Exit
+            #  NOTE: Allow the user's request when they type: 'Exit', 'Cancel', 'Return',
+            #         as well as 'X'.
+            #         This can come handy if the user is in a panic - remember that the terminal
+            #         is intimidating for some which may cause user's to panic, and this can be
+            #         helpful if user's are just used to typing 'Exit' or perhaps 'Quit'.
+            {($_ -eq "X") -or `
+                ($_ -eq "Exit") -or `
+                ($_ -eq "Cancel") -or `
+                ($_ -eq "Return")}
+            {
+                # Return back to the previous menu
+                return $false;
+            } # Exit
+
+
+
+            # Unknown Option
+            default
+            {
+                # Provide an error message to the user that the option they chose is
+                #  not available.
+                [CommonCUI]::DrawIncorrectMenuOption();
+
+
+                # Finished
+                break;
+            } # Unknown Option
+        } # Switch : Evaluate User's Request
+
+
+
+        # Finished with the operation; return back to the current menu.
+        return $true;
+    } # EvaluateExecuteUserRequestLocateGitPath()
+
+
+
+
+   <# Configure Locate Git Path
+    # -------------------------------
+    # Documentation:
+    #  This function will allow the user the ability to configure the path of
+    #   the Git directory.
+    # -------------------------------
+    #>
+    hidden static [void] LocateGitPathNewPath()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This will temporarily hold the user's requested path; if the path is valid -
+        #  then we will use the value already given from this variable to store it to the
+        #  Git Path variable.
+        [string] $newPath = $NULL;
+
+        # We will use this instance so that we can apply the new location to the object.
+        [GitControl] $gitControl = [GitControl]::GetInstance();
+        # ----------------------------------------
+
+
+
+        # Provide some extra spacing as we will continue to use the same content
+        #  area without clearing the terminal screen.  Thus, we are going to move
+        #  a few lines down and apply the newer content below.
+        [Logging]::DisplayMessage("`r`n`r`n");
+
+
+        # Determine if the path that were provided is valid and can be used by the program.
+        if ([CommonCUI]::BrowseForTargetFile([ref] $newPath))
+        {
+            # Because the path is valid, we will use the requested target directory.
+            $gitControl.SetExecutablePath("$($newPath)");
+        } # if: Path is valid
+
+        # The provided path is not valid
+        else
+        {
+            # If the user provided "Cancel" or "X", then do not bother the user with an error message.
+            #  Otherwise, provide an error message as the path is incorrect.
+            if (("$($newPath)" -ne "Cancel") -and `
+                ("$($newPath)" -ne "x"))
+            {
+                # Because the path is not valid, let the user know that the path does not exist
+                #  and will not be used.
+                [Logging]::DisplayMessage("`r`n" + `
+                                        "The provided path does not exist and cannot be used." + `
+                                        "`r`n`r`n");
+
+
+                # Wait for the user to provide feedback; thus allowing the user to see the message.
+                [Logging]::GetUserEnterKey();
+            } # if : User Provided incorrect path
+        } # else : Path is invalid
+    } # LocateGitPathNewPath()
+    #endregion
 } # SettingsGit
