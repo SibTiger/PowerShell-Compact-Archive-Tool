cdddc99eb36a930ad1950802dbd23746f328439b
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-05-02 20:36:26 -0500

    It is now possible for the user to specify the algorithm that will be used during a Zip compression operation.

--- scripts/Settings7Zip.ps1
+++ scripts/Settings7Zip.ps1
@@ -201,7 +201,8 @@ class Settings7Zip
             {($_ -eq "Z") -or `
                 ($_ -eq "Zip Algorithm")}
             {
-                # Still working on this
+                # Allow the user to configure the Zip algorithm
+                [Settings7Zip]::AlgorithmsZip()
 
 
                 # Finished
@@ -851,5 +852,274 @@ class Settings7Zip
         # Finished with the operation; return back to the current menu.
         return $true;
     } # EvaluateExecuteUserRequestCompressionMethod()
+    #endregion
+
+
+
+
+
+    #region Algorithms using Zip
+    #                                    Algorithms using Zip
+    # ==========================================================================================
+    # ------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------
+    # ==========================================================================================
+
+
+
+
+
+   <# Algorithms Zip
+    # -------------------------------
+    # Documentation:
+    #  This function will allow the user the ability to determine which algorithm will be enabled
+    #  while utilizing the Zip Compression Methodology within the program.
+    # -------------------------------
+    #>
+    hidden static [void] AlgorithmsZip()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This variable will hold the user's input as they navigate within the menu.
+        [string] $userInput = $null;
+
+        # This variable will determine if the user is to remain within the current menu loop.
+        #  If the user were to exit from the menu, this variable's state will be set as false.
+        #  Thus, with a false value - they may leave from the menu.
+        [bool] $menuLoop = $true;
+
+        # Retrieve the current instance of the 7Zip object; this contains the user's settings
+        #  when using the 7Zip application.
+        [SevenZip] $sevenZip = [SevenZip]::GetInstance();
+
+        # We will use this variable to make the string that is displayed to the user - a bit easier to read.
+        #  Further, we could use a simple if conditional statement below where we ultimately just display the
+        #  results, but lets keep the code nicer to read for our own benefit instead.
+        [string] $decipherNiceString = $null;
+        # ----------------------------------------
+
+        # Open the Algorithm Zip Configuration menu
+        #  Keep the user within the menu until they request to return back to the previous menu.
+        do
+        {
+            # Determine the current state of the Algorithm Zip variable, then make it easier for
+            #  the user to understand the current setting.
+            switch ($sevenZip.GetAlgorithmZip())
+            {
+                "Deflate"
+                {
+                    # Set the message such that the user knows that the 'Deflate' algorithm is currently set.
+                    $decipherNiceString = "I will have 7Zip use the Deflate algorithm when compiling new builds.";
+
+                    # Finished
+                    break;
+                } # Deflate
+
+                "LZMA"
+                {
+                    # Set the message such that the user knows that the 'LZMA' algorithm is currently set.
+                    $decipherNiceString = "I will have 7Zip use the LZMA algorithm when compiling new builds.";
+
+                    # Finished
+                    break;
+                } # LZMA
+
+                "BZip2"
+                {
+                    # Set the message such that the user knows that the 'BZip2' algorithm is currently set.
+                    $decipherNiceString = "I will have 7Zip use the BZip2 algorithm when compiling new builds.";
+
+                    # Finished
+                    break;
+                } # BZip2
+            } # Switch: Decipher Algorithm Zip
+
+
+
+            # Clear the terminal of all previous text; keep the space clean so that it is easy
+            #  for the user to read and follow along.
+            [CommonIO]::ClearBuffer();
+
+            # Draw Program Information Header
+            [CommonCUI]::DrawProgramTitleHeader();
+
+            # Show the user that they are at the Algorithms using Zip menu
+            [CommonCUI]::DrawSectionHeader("Algorithms using Zip");
+
+            # Show to the user the current state of the 'Algorithm Zip' variable that is presently set within the program.
+            [Logging]::DisplayMessage("$($decipherNiceString)");
+
+            # Provide some extra white spacing so that it is easier to read for the user
+            [Logging]::DisplayMessage("`r`n`r`n");
+
+            # Display the instructions to the user
+            [CommonCUI]::DrawMenuInstructions();
+
+            # Draw the menu list to the user
+            [Settings7Zip]::DrawMenuAlgorithmZip();
+
+            # Provide some extra padding
+            [Logging]::DisplayMessage("`r`n");
+
+            # Capture the user's feedback
+            $userInput = [CommonCUI]::GetUserInput("WaitingOnYourResponse");
+
+            # Execute the user's request
+            $menuLoop = [Settings7Zip]::EvaluateExecuteUserRequestAlgorithmZip($userInput);
+        } while ($menuLoop);
+    } # AlgorithmsZip()
+
+
+
+
+   <# Draw Menu: Compression Method
+    # -------------------------------
+    # Documentation:
+    #  This function will essentially draw the menu list for the Algorithm Zip to the user.
+    #   Thus, this provides what options are available in relation to the Algorithms for Zip in 7Zip.
+    # -------------------------------
+    #>
+    hidden static [void] DrawMenuAlgorithmZip()
+    {
+        # Display the Menu List
+
+        # Deflate
+        [CommonCUI]::DrawMenuItem('D', `
+                                "Deflate", `
+                                "$($NULL)", `
+                                $true);
+
+
+        # LZMA
+        [CommonCUI]::DrawMenuItem('L', `
+                                "LZMA", `
+                                "$($NULL)", `
+                                $true);
+
+
+        # BZip2
+        [CommonCUI]::DrawMenuItem('B', `
+                                "BZip2", `
+                                "$($NULL)", `
+                                $true);
+
+
+        # Return back to the previous menu
+        [CommonCUI]::DrawMenuItem('X', `
+                                "Cancel", `
+                                "Return back to the previous menu.", `
+                                $true);
+    } # DrawMenuAlgorithmZip()
+
+
+
+
+   <# Evaluate and Execute User's Request: Algorithm Zip
+    # -------------------------------
+    # Documentation:
+    #  This function will evaluate and execute the user's desired request in respect to
+    #   the menu options that were provided to them.
+    # -------------------------------
+    # Input:
+    #  [string] User's Request
+    #   This will provide the user's desired request to run an operation or to access
+    #    a specific functionality.
+    # -------------------------------
+    # Output:
+    #  [bool] User Stays at Menu
+    #   This defines if the user is to remain at the Menu screen.
+    #       $true  = User is to remain at the Menu.
+    #       $false = User requested to leave the Menu.
+    # -------------------------------
+    #>
+    hidden static [bool] EvaluateExecuteUserRequestAlgorithmZip([string] $userRequest)
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # We will use this instance so that we can apply the new changes to the object.
+        [SevenZip] $sevenZip = [SevenZip]::GetInstance();
+        # ----------------------------------------
+
+
+        # Evaluate the user's request
+        switch ($userRequest)
+        {
+            # Change the Zip Algorithm to Deflate
+            #  NOTE: Allow the user's request when they type: 'Deflate' or 'D'.
+            {($_ -eq "D") -or `
+                ($_ -eq "Deflate")}
+            {
+                # Use the Deflate Algorithm when using the Zip Compression Methodology
+                $sevenZip.SetAlgorithmZip([SevenZipAlgorithmZip]::Deflate);
+
+
+                # Finished
+                break;
+            } # Selected Deflate Algorithm
+
+
+            # Change the Zip Algorithm to LZMA
+            #  NOTE: Allow the user's request when they type: 'LZMA' or 'L'.
+            {($_ -eq "L") -or `
+                ($_ -eq "LZMA")}
+            {
+                # Use the LZMA Algorithm when using the Zip Compression Methodology
+                $sevenZip.SetAlgorithmZip([SevenZipAlgorithmZip]::LZMA);
+
+
+                # Finished
+                break;
+            } # Selected LZMA Compression
+
+
+            # Change the Zip Algorithm to BZip2
+            #  NOTE: Allow the user's request when they type: 'BZip2' or 'B'.
+            {($_ -eq "B") -or `
+                ($_ -eq "BZip2")}
+            {
+                # Use the BZip2 Algorithm when using the Zip Compression Methodology
+                $sevenZip.SetAlgorithmZip([SevenZipAlgorithmZip]::BZip2);
+
+
+                # Finished
+                break;
+            } # Selected BZip2 Compression
+
+
+            # Exit
+            #  NOTE: Allow the user's request when they type: 'Exit', 'Cancel', 'Return',
+            #         as well as 'X'.
+            #         This can come handy if the user is in a panic - remember that the terminal
+            #         is intimidating for some which may cause user's to panic, and this can be
+            #         helpful if user's are just used to typing 'Exit' or perhaps 'Quit'.
+            {($_ -eq "X") -or `
+                ($_ -eq "Exit") -or `
+                ($_ -eq "Cancel") -or `
+                ($_ -eq "Return")}
+            {
+                # Return back to the previous menu
+                return $false;
+            } # Exit
+
+
+
+            # Unknown Option
+            default
+            {
+                # Provide an error message to the user that the option they chose is
+                #  not available.
+                [CommonCUI]::DrawIncorrectMenuOption();
+
+
+                # Finished
+                break;
+            } # Unknown Option
+        } # Switch : Evaluate User's Request
+
+
+
+        # Finished with the operation; return back to the current menu.
+        return $true;
+    } # EvaluateExecuteUserRequestAlgorithmZip()
     #endregion
 } # Settings7Zip
