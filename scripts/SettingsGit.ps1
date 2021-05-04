aec59c938d8855476be8c59468e4515a8faabcf5
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-05-03 19:00:00 -0500

    It is now possible for the user to allow the program to update their source files.

--- scripts/SettingsGit.ps1
+++ scripts/SettingsGit.ps1
@@ -172,6 +172,24 @@ class SettingsGit
 
 
 
+            # Update Source
+            #  NOTE: Allow the user's request when they type: 'Update Source', 'Update Project',
+            #           'Update', as well as 'U'.
+            {($_ -eq "U") -or `
+                ($_ -eq "Update") -or `
+                ($_ -eq "Update Source") -or `
+                ($_ -eq "Update Project")}
+            {
+                # Allow the user the ability to choose if they want to update the project's
+                #  source files or not to update the project's files.
+                [SettingsGit]::UpdateSource()
+
+
+                # Finished
+                break;
+            } # Update Source
+
+
             # Access the Help Program's Documentation
             #  NOTE: Allow the user's request when they type: 'Help', 'Helpme',
             #           'Help me', as well as '?'.
@@ -490,5 +508,243 @@ class SettingsGit
             } # if : User Provided incorrect path
         } # else : Path is invalid
     } # LocateGitPathNewPath()
+    #endregion
+
+
+
+
+
+    #region Update Source
+    #                                     Update Source
+    # ==========================================================================================
+    # ------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------
+    # ==========================================================================================
+
+
+
+
+
+   <# Update Source
+    # -------------------------------
+    # Documentation:
+    #  This function will allow the user the ability to customize the Update Source functionality.
+    #   With this setting, the program can try to update the project's source files to the latest
+    #   commits from the remote master.
+    # -------------------------------
+    #>
+    hidden static [void] UpdateSource()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This variable will hold the user's input as they navigate within the menu.
+        [string] $userInput = $null;
+
+        # This variable will determine if the user is to remain within the current menu loop.
+        #  If the user were to exit from the menu, this variable's state will be set as false.
+        #  Thus, with a false value - they may leave from the menu.
+        [bool] $menuLoop = $true;
+
+        # Retrieve the current instance of the Git object; this contains the user's settings
+        #  when using the Git application.
+        [GitControl] $gitControl = [GitControl]::GetInstance();
+
+        # We will use this variable to make the string that is displayed to the user - a bit easier to read.
+        #  Further, we could use a simple if conditional statement below where we ultimately just display the
+        #  results, but lets keep the code nicer to read for our own benefit instead.
+        [string] $decipherNiceString = $null;
+        # ----------------------------------------
+
+        # Open the Update Source Configuration menu
+        #  Keep the user within the menu until they request to return back to the previous menu.
+        do
+        {
+            # Determine the current state of the Update Source variable, then make it easier
+            #  for the user to understand the current setting.
+            # Is the Program supposed to update the project's source files?
+            if ($gitControl.GetUpdateSource())
+            {
+                # Set the message such that the user knows that this program will try to
+                #  update the project's source files.
+                $decipherNiceString = "I will try to update the project's source files when possible to the latest changes.";
+            } # if : Update the Project's Source Files
+
+            # The program is not supposed to update the project's source files
+            else
+            {
+                # Set the message such that the user knows that this program will not try to update the project's source files.
+                $decipherNiceString = "I will not update the project's source files."
+            } # else : Do not Update the Project's Source Files
+
+
+            # Clear the terminal of all previous text; keep the space clean so that it is easy
+            #  for the user to read and follow along.
+            [CommonIO]::ClearBuffer();
+
+            # Draw Program Information Header
+            [CommonCUI]::DrawProgramTitleHeader();
+
+            # Show the user that they are at the Update Source menu
+            [CommonCUI]::DrawSectionHeader("Update Project Files");
+
+            # Show to the user the current state of the Update Source presently set within the program
+            [Logging]::DisplayMessage("$($decipherNiceString)");
+
+            # Provide some extra white spacing so that it is easier to read for the user
+            [Logging]::DisplayMessage("`r`n`r`n");
+
+            # Display the instructions to the user
+            [CommonCUI]::DrawMenuInstructions();
+
+            # Draw the menu list to the user
+            [SettingsGit]::DrawMenuUpdateSource();
+
+            # Provide some extra padding
+            [Logging]::DisplayMessage("`r`n");
+
+            # Capture the user's feedback
+            $userInput = [CommonCUI]::GetUserInput("WaitingOnYourResponse");
+
+            # Execute the user's request
+            $menuLoop = [SettingsGit]::EvaluateExecuteUserRequestUpdateSource($userInput);
+        } while ($menuLoop);
+    } # UpdateSource()
+
+
+
+
+   <# Draw Menu: Update Source
+    # -------------------------------
+    # Documentation:
+    #  This function will essentially provide the menu list regarding the Update Source Files
+    #  functionality.
+    # -------------------------------
+    #>
+    hidden static [void] DrawMenuUpdateSource()
+    {
+        # Display the Menu List
+
+        # Enable the Update Source feature
+        [CommonCUI]::DrawMenuItem('U', `
+                                "Update Project's Source Files", `
+                                "Update the project to the current changes available on the remote master server.", `
+                                $true);
+
+
+        # Disable the Update Source feature
+        [CommonCUI]::DrawMenuItem('N', `
+                                "Do not Update Project's Source Files", `
+                                "Do not update the project's source files with the latest changes made from the remote master server.", `
+                                $true);
+
+
+        # Return back to the previous menu
+        [CommonCUI]::DrawMenuItem('X', `
+                                "Cancel", `
+                                "Return back to the previous menu.", `
+                                $true);
+    } # DrawMenuUpdateSource()
+
+
+
+
+   <# Evaluate and Execute User's Request: Locate Git Path
+    # -------------------------------
+    # Documentation:
+    #  This function will evaluate and execute the user's desired request in respect to
+    #   the menu options that were provided to them.
+    # -------------------------------
+    # Input:
+    #  [string] User's Request
+    #   This will provide the user's desired request to run an operation or to access
+    #    a specific functionality.
+    # -------------------------------
+    # Output:
+    #  [bool] User Stays at Menu
+    #   This defines if the user is to remain at the Menu screen.
+    #       $true  = User is to remain at the Menu.
+    #       $false = User requested to leave the Menu.
+    # -------------------------------
+    #>
+    hidden static [bool] EvaluateExecuteUserRequestUpdateSource([string] $userRequest)
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # Retrieve the current instance of the Git object; this contains the user's settings
+        #  when using the Git application.
+        [GitControl] $gitControl = [GitControl]::GetInstance();
+        # ----------------------------------------
+
+
+        # Evaluate the user's request
+        switch ($userRequest)
+        {
+            # Allow the Project Source to be Updated
+            #  NOTE: Allow the user's request when they type: 'Update', 'Update Project',
+            #           as well as 'U'.
+            {($_ -eq "U") -or `
+                ($_ -eq "Update") -or `
+                ($_ -eq "Update Project")}
+            {
+                # Allow the ability to update the project' source files.
+                $gitControl.SetUpdateSource($true);
+
+
+                # Finished
+                break;
+            } # Update Source
+
+
+            # Disallow the Project Source to be Updated
+            #  NOTE: Allow the user's request when they type: 'Do not update', 'Do not Update Project',
+            #           as well as 'N'.
+            {($_ -eq "N") -or `
+                ($_ -eq "Do not update") -or `
+                ($_ -eq "Do not update Project")}
+            {
+                # Allow the ability to update the project' source files.
+                $gitControl.SetUpdateSource($false);
+
+
+                # Finished
+                break;
+            } # Do not Update Source
+
+
+            # Exit
+            #  NOTE: Allow the user's request when they type: 'Exit', 'Cancel', 'Return',
+            #         as well as 'X'.
+            #         This can come handy if the user is in a panic - remember that the terminal
+            #         is intimidating for some which may cause user's to panic, and this can be
+            #         helpful if user's are just used to typing 'Exit' or perhaps 'Quit'.
+            {($_ -eq "X") -or `
+                ($_ -eq "Exit") -or `
+                ($_ -eq "Cancel") -or `
+                ($_ -eq "Return")}
+            {
+                # Return back to the previous menu
+                return $false;
+            } # Exit
+
+
+
+            # Unknown Option
+            default
+            {
+                # Provide an error message to the user that the option they chose is
+                #  not available.
+                [CommonCUI]::DrawIncorrectMenuOption();
+
+
+                # Finished
+                break;
+            } # Unknown Option
+        } # Switch : Evaluate User's Request
+
+
+
+        # Finished with the operation; return back to the current menu.
+        return $true;
+    } # EvaluateExecuteUserRequestUpdateSource()
     #endregion
 } # SettingsGit
