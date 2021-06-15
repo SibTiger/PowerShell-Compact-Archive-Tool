ef9c2695385093eafb199a9589c7ad5e5c5d2fa3
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2021-06-14 20:43:36 -0500

    Created the Builder Class with no meat
    Quickly created the Builder class - though only the object documentation and the class itself with no meat.  I will slowly add more contents as the week progresses.

---
+++ scripts/Builder.ps1
@@ -0,0 +1,21 @@
+<# Builder
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This class is the heart of the program - in which it allows the
+ #  desired project to be compiled into a single archive data file.
+ #  The builder will perform various steps to assure that the project
+ #  is up to date with the remote repository, assure that all of the
+ #  dependencies are ready for use, perform the compiling operation,
+ #  verify the integrity of the newly generated archive datafile,
+ #  and create any useful documentation as requested.  The user has
+ #  full flexibility as to how the Builder operates and with what
+ #  capabilities are usable.
+ #>
+
+
+
+class Builder
+{
+
+} # Builder
