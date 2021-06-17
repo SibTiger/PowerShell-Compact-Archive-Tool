06379b68dddc2495bf11c0d032f37cdf3fcd9ad3
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2021-06-16 20:08:33 -0500

    Added a new function, PrerequisiteCheck()
    Added a new function into the Builder, PrerequisiteCheck().  This function will assure that all of the intended resources are presently available and ready for us to utilize during the compile phase.  If we are missing one resource - then this function will report a $false.

--- scripts/Builder.ps1
+++ scripts/Builder.ps1
@@ -42,7 +42,112 @@ class Builder
     #>
     static [bool] Build ()
     {
+        # Before we begin, we must make sure that the required resources
+        #  are available for us to use within this operation.
+        
         # To avoid compiling issues, we will merely return an error for now.
         return $false;
     } # Builder()
+
+
+
+
+   <# Prerequisite Check
+    # -------------------------------
+    # Documentation:
+    #  The function performs a validation check to assure that all
+    #   of the required resources are available for the compiling
+    #   process.  If incase we found one or more resources - that
+    #   is imperative for the entire operation to work correctly -
+    #   then we may ultimately abruptly abort the entire compile
+    #   operation.
+    # -------------------------------
+    # Output:
+    #  [bool] Exit code
+    #   $false = One or more resources were missing but required.
+    #   $true = All of the resources were accounted for and ready.
+    # -------------------------------
+    #>
+    hidden static [bool] PrerequisiteCheck()
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # Retrieve the current instance of the User Preferences object; this contains the user's
+        #  generalized settings.
+        [UserPreferences] $userPreferences = [UserPreferences]::GetInstance();
+        # ----------------------------------------
+
+
+        # Can we find the project's source files?
+        if (![CommonIO]::CheckPathExists("$($userPreferences.GetProjectPath())", $true))
+        {
+            # Unable to find the project's source files; unable to continue.
+            return $false;
+        } # if : Check Project source files exists
+
+
+        # Can we find the output path?
+        if (![CommonIO]::CheckPathExists("$($userPreferences.GetProjectBuildsPath())", $true))
+        {
+            # Unable to find the output path directory; unable to continue.
+            return $false;
+        } # if : Check Output Path exists
+
+
+        # Determine if the chosen compression tool is available for us to utilize.
+        switch ($userPreferences.GetCompressionTool())
+        {
+            # dotNET Archive Zip
+            [UserPreferencesCompressTool]::InternalZip
+            {
+                # Make sure that the dotNET Archive Zip is available
+                if (![CommonFunctions]::IsAvailableZip())
+                {
+                    # Unable to find the dotNET Archive Zip
+                    return $false;
+                } # If : Found Default Zip
+
+                # Finished
+                break;
+            } # dotNET Archive Zip
+
+            # 7Zip
+            [UserPreferencesCompressTool]::SevenZip
+            {
+                # Make sure that the 7Zip is available
+                if (![CommonFunctions]::IsAvailable7Zip())
+                {
+                    # Unable to find the 7Zip application
+                    return $false;
+                } # If : Found 7Zip
+
+                # Finished
+                break;
+            } # 7Zip
+
+            # Unknown or Unsupported (Error Case)
+            default
+            {
+                # Unknown or Unsupported compression tool!
+                return $false;
+            } # Unknown or Unsupported
+        } # Switch : Determine Specified Compression Tool
+
+
+        # Determine if the user wanted us to use Git Features
+        if ($userPreferences.GetUseGitFeatures())
+        {
+            # Assure that we are able to find the Git Application
+            if (![CommonFunctions]::IsAvailableGit())
+            {
+                # Unable to find the Git application.
+                return $false;
+            } # if : Check if Git Exists
+        } # if : Git Features Requested
+
+
+        # If we made it to this point, then we have all of the resources
+        #  that we need to compile this project!
+        return $true;
+    } # PrerequisiteCheck()
 } # Builder
