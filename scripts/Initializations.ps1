d1b14b104eb9a49655dac6b4191ec7c2dbb328f3
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-07-04 11:54:14 -0500

    Upped the program version to 1.0.0 Alpha
    We are getting closer :wink:

--- scripts/Initializations.ps1
+++ scripts/Initializations.ps1
@@ -50,7 +50,7 @@ function InitializationProgramIdentity()
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
     # Version
-    Set-Variable -Name "_VERSION_" -Value "0.0.9001-Alpha" `
+    Set-Variable -Name "_VERSION_" -Value "1.0.0 Alpha" `
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
     # Version Name
