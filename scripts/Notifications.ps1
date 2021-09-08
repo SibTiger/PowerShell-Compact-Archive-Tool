facac3829ac9b51675708ced4353ce0b6d78d7ef
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-09-07 19:42:24 -0500

    [WIP] Added a new file, Notifications.
    This will allow the ability to provide notifications to the user, if allowed.

---
+++ scripts/Notifications.ps1
@@ -0,0 +1,18 @@
+<# Notifications
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This class will provide the ability to notify the user based on certain events.
+ #  The notifications provided may result in a basic tone, while other notifications
+ #  may provide a audio alerts and\or visual notifications.  The purpose of this
+ #  functionality is to grab the user's attention that /something/ had happened.
+ #  Further, in order for the notification functionality to work, the user must
+ #  had already configured their settings to allow such capabilities.
+ #>
+
+
+
+class Notifications
+{
+
+} # Notifications
