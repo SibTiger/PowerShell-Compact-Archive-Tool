f6fc7251bc200283776bf78aa99d427fd113946e
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-09-10 19:49:04 -0500

    Added standard system sounds: Beep, Asterisk, Exclamation, Hand, and Question

--- scripts/Notifications.ps1
+++ scripts/Notifications.ps1
@@ -8,6 +8,10 @@
  #  functionality is to grab the user's attention that /something/ had happened.
  #  Further, in order for the notification functionality to work, the user must
  #  had already configured their settings to allow such capabilities.
+ #
+ # Useful Resources:
+ #  Standard System Sounds: https://stackoverflow.com/a/31265651/11314373
+ #  MS: https://docs.microsoft.com/en-us/dotnet/api/system.media.systemsounds
  #>
 
 
@@ -55,4 +59,103 @@ class Notifications
         # Ordinary bell
         [System.Console]::Beep();
     } # StandardBell()
+
+
+
+
+
+   <# PLAY: Beep Bell
+    # -------------------------------
+    # Documentation:
+    #  This will function will play the 'Beep' notification provided by the
+    #   Operating System, if supported.
+    # -------------------------------
+    #>
+    static hidden [void] PlaySoundBeep()
+    {
+        # Play the Beep sound
+        [System.Media.SystemSounds]::Beep.Play();
+    } # PlaySoundBeep()
+
+
+
+
+
+   <# PLAY: Asterisk Bell
+    # -------------------------------
+    # Documentation:
+    #  This will function will play the 'Asterisk' notification provided by the
+    #   Operating System, if supported.
+    #
+    # Ideally to be played when an event had been reached.
+    # -------------------------------
+    #>
+    static hidden [void] PlaySoundAsterisk()
+    {
+        # Play the Asterisk sound
+        [System.Media.SystemSounds]::Asterisk.Play();
+    } # PlaySoundAsterisk()
+
+
+
+
+
+   <# PLAY: Exclamation Bell
+    # -------------------------------
+    # Documentation:
+    #  This will function will play the 'Exclamation' notification provided by the
+    #   Operating System, if supported.
+    #
+    # Ideally to be played when something of a great importance had occurred.
+    #  GUI: Used with the Exclamation Icon
+    # -------------------------------
+    #>
+    static hidden [void] PlaySoundExclamation()
+    {
+        # Play the Exclamation sound
+        [System.Media.SystemSounds]::Exclamation.Play();
+    } # PlaySoundExclamation()
+
+
+
+
+
+   <# PLAY: Hand Bell
+    # -------------------------------
+    # Documentation:
+    #  This will function will play the 'Hand' notification provided by the
+    #   Operating System, if supported.
+    #
+    # Ideally to be played when a 'Critical Stop' had been reached.
+    # -------------------------------
+    #>
+    static hidden [void] PlaySoundHand()
+    {
+        # Play the Critical Stop sound
+        [System.Media.SystemSounds]::Hand.Play();
+    } # PlaySoundHand()
+
+
+
+
+
+   <# PLAY: Question Bell
+    # -------------------------------
+    # Documentation:
+    #  This will function will play the 'Question' notification provided by the
+    #   Operating System, if supported.
+    #
+    # Ideally to be played when a 'Question' had arisen to the user.
+    # Developer Note:
+    #  I do not think this is being used presently?  As of writing this function,
+    #   it really does virtually nothing useful.  Think of this as a Pointless Button
+    #   from the ASDF Movie on YouTube, it is exactly that.  Hmm
+    #   https://youtu.be/4aK3P-n1aWk
+    # -------------------------------
+    #>
+    static hidden [void] PlaySoundQuestion()
+    {
+        # Play the Questionable sound
+        [System.Media.SystemSounds]::Question.Play();
+    } # PlaySoundQuestion()
 } # Notifications
