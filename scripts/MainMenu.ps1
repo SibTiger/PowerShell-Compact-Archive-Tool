0d7bd4edc2b02d3b00b4c4d2af80e4536fef2263
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2021-09-19 12:27:27 -0500

    Add an audible notification to the user if the page could not open successfully due to an error.

--- scripts/MainMenu.ps1
+++ scripts/MainMenu.ps1
@@ -226,11 +226,13 @@ class MainMenu
                 ($_ -eq "$([ProjectInformation]::projectName) Homepage")}
             {
                 # Open the webpage as requested
-                #  NOTE: We do not care about the return result as there's
-                #         nothing we can do at this present point.
-                [WebsiteResources]::AccessWebSite_General([ProjectInformation]::urlWebsite,                 ` # Project's Homepage
-                                                        "$([ProjectInformation]::projectName) Homepage",    ` # Show page title
-                                                        $false) | Out-Null;                                 ` # Do not force Web Browser function
+                if (![WebsiteResources]::AccessWebSite_General([ProjectInformation]::urlWebsite,                ` # Project's Homepage
+                                                            "$([ProjectInformation]::projectName) Homepage",    ` # Show page title
+                                                            $false))                                            ` # Do not force Web Browser functionality.
+                {
+                    # Alert the user that the web functionality did not successfully work as intended.
+                    [Notifications]::Notify([NotificationEventType]::Error);
+                } # If : Failed to Provide Webpage
 
 
                 # Finished
