c649832c2ef69264735fcc4fb569f58ee43938da
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2021-11-28 11:47:31 -0600

    Version 1.0.0

--- scripts/Initializations.ps1
+++ scripts/Initializations.ps1
@@ -70,7 +70,7 @@ function InitializationProgramIdentity()
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
     # Version
-    Set-Variable -Name "_VERSION_" -Value "1.0.0 [Beta]" `
+    Set-Variable -Name "_VERSION_" -Value "1.0.0" `
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 
     # Version Name
@@ -79,7 +79,7 @@ function InitializationProgramIdentity()
 
     # Release Date
     # Date Format: DD.MM.YYYY with leading zeros
-    Set-Variable -Name "_RELEASEDATE_" -Value "30.10.2021" `
+    Set-Variable -Name "_RELEASEDATE_" -Value "28.11.2021" `
         -Option Constant -Scope Global -ErrorAction SilentlyContinue;
 } # InitializationProgramIdentity()
 
