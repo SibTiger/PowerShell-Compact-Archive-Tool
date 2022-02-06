c6c62d1dde0e19d63cfcce60e3c3f167c7f34486
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2022-02-05 23:51:45 -0600

    Revised the copyright year

--- scripts/help.ps1
+++ scripts/help.ps1
@@ -1,5 +1,5 @@
 ﻿<# PowerShell Compact-Archive Tool
- # Copyright (C) 2021
+ # Copyright (C) 2022
  #
  # This program is free software: you can redistribute it and/or modify
  # it under the terms of the GNU General Public License as published by
