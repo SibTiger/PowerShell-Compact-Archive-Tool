5f479f1a72d60e09c808f81c471e036972fbf987
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2022-03-06 21:19:43 -0600

    update Uninitializations.ps1

--- scripts/Uninitializations.ps1
+++ scripts/Uninitializations.ps1
@@ -75,59 +75,59 @@
         -Scope Global -Force;
 
     # Program Source Repository
-    Remove-Variable -Name "_PROGRAMSITESOURCEREPOSITORY_"
+    Remove-Variable -Name "_PROGRAMSITESOURCEREPOSITORY_" `
 
     # Report a Bug or Feature
-    Remove-Variable -Name "_PROGRAMREPORTBUGORFEATURE_"
+    Remove-Variable -Name "_PROGRAMREPORTBUGORFEATURE_" `
 
     # .NET Framework Requirement
-    Remove-Variable -Name "_DOTNETFRAMEWORK_"
+    Remove-Variable -Name "_DOTNETFRAMEWORK_" `
 
     # PowerShell Version Requirement
-    Remove-Variable -Name "_POWERSHELLVERSION_"
+    Remove-Variable -Name "_POWERSHELLVERSION_" `
 
     # Debug Logging functionality
-    Remove-Variable -Name "_DEBUGLOGGING_"
+    Remove-Variable -Name "_DEBUGLOGGING_" `
 
     # Logging Lock Key
-    Remove-Variable -Name "_LOGGINGLOCKKEY_"
+    Remove-Variable -Name "_LOGGINGLOCKKEY_" `
 
     # Splash Screen Hold Timer (Seconds)
-    Remove-Variable -Name "_STARTUPSPLASHSCREENHOLDTIME_"
+    Remove-Variable -Name "_STARTUPSPLASHSCREENHOLDTIME_" `
 
     # Script Absolute Script Path
-    Remove-Variable -Name "_SCRIPTPATH_"
+    Remove-Variable -Name "_SCRIPTPATH_" `
 
     # User-Data Root Directory Path
-    Remove-Variable -Name "_USERDATA_ROOT_PATH_"
+    Remove-Variable -Name "_USERDATA_ROOT_PATH_" `
 
     # User-Data Project Parent Project Directory Path
-    Remove-Variable -Name "_USERDATA_PROJECT_PATH_"
+    Remove-Variable -Name "_USERDATA_PROJECT_PATH_" `
 
     # Output Parent Directory Path
-    Remove-Variable -Name "_USERDATA_BUILDS_PATH_"
+    Remove-Variable -Name "_USERDATA_BUILDS_PATH_" `
 
     # Output Release Directory Path
-    Remove-Variable -Name "_USERDATA_RELEASEBUILDS_PATH_"
+    Remove-Variable -Name "_USERDATA_RELEASEBUILDS_PATH_" `
 
     # Output Development Directory Path
-    Remove-Variable -Name "_USERDATA_DEVBUILDS_PATH_"
+    Remove-Variable -Name "_USERDATA_DEVBUILDS_PATH_" `
 
     # Program-Data Root Directory Path
-    Remove-Variable -Name "_PROGRAMDATA_ROOT_LOCAL_PATH_"
+    Remove-Variable -Name "_PROGRAMDATA_ROOT_LOCAL_PATH_" `
 
     # Program-Data Project Parent Directory Path
-    Remove-Variable -Name "_PROGRAMDATA_PROJECT_PATH_"
+    Remove-Variable -Name "_PROGRAMDATA_PROJECT_PATH_" `
 
     # Log Directory Path
-    Remove-Variable -Name "_PROGRAMDATA_LOGS_PATH_"
+    Remove-Variable -Name "_PROGRAMDATA_LOGS_PATH_" `
 
     # Program-Data Parent Directory Path
-    Remove-Variable -Name "_PROGRAMDATA_ROOT_ROAMING_PATH_"
+    Remove-Variable -Name "_PROGRAMDATA_ROOT_ROAMING_PATH_" `
 
     # User Data (Configuration)
-    Remove-Variable -Name "_PROGRAMDATA_CONFIGS_PATH_"
+    Remove-Variable -Name "_PROGRAMDATA_CONFIGS_PATH_" `
 
     # PowerShell's Window Title
-    Remove-Variable -Name "_ENVIRONMENT_WINDOW_TITLE_ORIGINAL_"
+    Remove-Variable -Name "_ENVIRONMENT_WINDOW_TITLE_ORIGINAL_" `
  } # Uninitializations()
