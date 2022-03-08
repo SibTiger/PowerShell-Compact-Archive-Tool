590d56c6e88bece8860c8a202ce88c4ba404651e
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2022-03-07 20:34:35 -0600

    Added 'Error Action' if incase an error where to occur
    If an error where to be reached, then it will now be silently ignored.

--- scripts/Uninitializations.ps1
+++ scripts/Uninitializations.ps1
@@ -38,140 +38,168 @@
     # Program Name
     Remove-Variable -Name "_PROGRAMNAME_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program Name (Abbreviated)
     Remove-Variable -Name "_PROGRAMNAMESHORT_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Version
     Remove-Variable -Name "_VERSION_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Version Name
     Remove-Variable -Name "_VERSIONNAME_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Release Date
     Remove-Variable -Name "_RELEASEDATE_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # License
     Remove-Variable -Name "_LICENSE_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program Homepage
     Remove-Variable -Name "_PROGRAMSITEHOMEPAGE_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program Wiki
     Remove-Variable -Name "_PROGRAMSITEWIKI_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program Downloads
     Remove-Variable -Name "_PROGRAMSITEDOWNLOADS_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program Source Repository
     Remove-Variable -Name "_PROGRAMSITESOURCEREPOSITORY_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Report a Bug or Feature
     Remove-Variable -Name "_PROGRAMREPORTBUGORFEATURE_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # .NET Framework Requirement
     Remove-Variable -Name "_DOTNETFRAMEWORK_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # PowerShell Version Requirement
     Remove-Variable -Name "_POWERSHELLVERSION_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Debug Logging functionality
     Remove-Variable -Name "_DEBUGLOGGING_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Logging Lock Key
     Remove-Variable -Name "_LOGGINGLOCKKEY_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Splash Screen Hold Timer (Seconds)
     Remove-Variable -Name "_STARTUPSPLASHSCREENHOLDTIME_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Script Absolute Script Path
     Remove-Variable -Name "_SCRIPTPATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # User-Data Root Directory Path
     Remove-Variable -Name "_USERDATA_ROOT_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # User-Data Project Parent Project Directory Path
     Remove-Variable -Name "_USERDATA_PROJECT_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Output Parent Directory Path
     Remove-Variable -Name "_USERDATA_BUILDS_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Output Release Directory Path
     Remove-Variable -Name "_USERDATA_RELEASEBUILDS_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Output Development Directory Path
     Remove-Variable -Name "_USERDATA_DEVBUILDS_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program-Data Root Directory Path
     Remove-Variable -Name "_PROGRAMDATA_ROOT_LOCAL_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program-Data Project Parent Directory Path
     Remove-Variable -Name "_PROGRAMDATA_PROJECT_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Log Directory Path
     Remove-Variable -Name "_PROGRAMDATA_LOGS_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # Program-Data Parent Directory Path
     Remove-Variable -Name "_PROGRAMDATA_ROOT_ROAMING_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # User Data (Configuration)
     Remove-Variable -Name "_PROGRAMDATA_CONFIGS_PATH_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
 
     # PowerShell's Window Title
     Remove-Variable -Name "_ENVIRONMENT_WINDOW_TITLE_ORIGINAL_" `
         -Scope Global `
-        -Force;
+        -Force `
+        -ErrorAction SilentlyContinue;
  } # Uninitializations()
