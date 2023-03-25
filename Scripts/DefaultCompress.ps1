cab53661c033389f482795db250d60dff926c965
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2023-03-25 14:28:50 -0600

    Changed the copyright date from 2022 to 2023

--- Scripts/DefaultCompress.ps1
+++ Scripts/DefaultCompress.ps1
@@ -1,5 +1,5 @@
 ﻿<# PowerShell Compact-Archive Tool
- # Copyright (C) 2022
+ # Copyright (C) 2023
  #
  # This program is free software: you can redistribute it and/or modify
  # it under the terms of the GNU General Public License as published by
