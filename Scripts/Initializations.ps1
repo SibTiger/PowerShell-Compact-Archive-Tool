d82905b80348f7b5788aaac298ab1b678fa2c402
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2023-03-25 19:26:50 -0600

    WIP - Updated the program's version and release date
    Updated the program version to: 1.2.0 [Alpha]
    Updated the program release date to: DD.MM.2023
    
    There's still plenty of work to do in this version

--- Scripts/Initializations.ps1
+++ Scripts/Initializations.ps1
@@ -73,7 +73,7 @@ function InitializationProgramIdentity()
         -Description "The short name of the application";
 
     # Version
-    Set-Variable -Name "_VERSION_" -Value "1.1.0" `
+    Set-Variable -Name "_VERSION_" -Value "1.2.0 [Alpha]" `
         -Option ReadOnly -Scope Global -ErrorAction SilentlyContinue `
         -Visibility Public `
         -Description "The version of the application";
@@ -87,7 +87,7 @@ function InitializationProgramIdentity()
 
     # Release Date
     # Date Format: DD.MM.YYYY with leading zeros
-    Set-Variable -Name "_RELEASEDATE_" -Value "05.02.2023" `
+    Set-Variable -Name "_RELEASEDATE_" -Value "DD.MM.2023" `
         -Option ReadOnly -Scope Global -ErrorAction SilentlyContinue `
         -Visibility Public `
         -Description "The date in which the version of the application was released.";
