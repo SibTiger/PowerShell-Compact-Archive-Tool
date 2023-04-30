525f1a515d6525c7758b596dc3f7e445f3e0e8f6
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2023-04-29 21:37:18 -0600

    Nitpicky spacing

--- Scripts/CommonGUI.ps1
+++ Scripts/CommonGUI.ps1
@@ -304,7 +304,7 @@ class CommonGUI
 
         # If we made it this far, then we know that the user had chosen one or many files.
         #  Provide the file(s) that the user had selected.
-        foreach ($i in $fileBrowser.FileNames){$files.Add($i);}
+        foreach ($i in $fileBrowser.FileNames) { $files.Add($i); }
 
 
         # Let the calling function know that the user provided atleast one file.
