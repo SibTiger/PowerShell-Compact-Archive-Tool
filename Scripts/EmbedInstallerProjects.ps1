5405f441318a0cd766e9db9eb4bd54e328841285
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2023-06-22 23:33:08 -0600

    WIP Created new file, EmbedInstallerProjects
    Created a new empty file, EmbedInstallerProjects.  Because there is going to be A LOT of work to maintain the project installation procedure, it is best to separate it from the EmbedInstaller class.

---
+++ Scripts/EmbedInstallerProjects.ps1
@@ -0,0 +1,45 @@
+<# PowerShell Compact-Archive Tool
+ # Copyright (C) 2023
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ #>
+
+
+
+
+<# Embed Installer - Projects
+ # ------------------------------
+ # ==============================
+ # ==============================
+ # This class provides the ability to install new projects into the program's environment.
+ #  As time continues, new projects and improvements with existing will be made available,
+ #  this class needs to meet with these needs.  Further, we need to allow a simple guided
+ #  steps to perform these actions.  Due to the nature of the terminal - the user will be
+ #  intimidated with the console window with limited-ease, compared to a standard GUI
+ #  application.  With this fact, we need to be sure we automate the process in such a
+ #  way that it is easy for the user - which they just want to get work done and deal with
+ #  no fuss.
+ #
+ # DEVELOPER NOTES:
+ #  We will rely heavily on the CommonGUI and CommonIO in order to make this
+ #  functionality easy for the user.
+ #>
+
+
+
+
+ class EmbedInstallerProjects
+ {
+    
+ } # EmbedInstallerProjects
