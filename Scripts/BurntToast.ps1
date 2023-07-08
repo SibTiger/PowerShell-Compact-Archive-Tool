e9c63a09ab8d5bd5d2eb28760bc285126e48416a
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2023-07-07 18:01:08 -0600

    ShowProjectMessage() will always show Logo Image
    Because of how the Logo is always shown in the Windows Toast notifications, ShowProjectMessage() will now honor that same methodology.

--- Scripts/BurntToast.ps1
+++ Scripts/BurntToast.ps1
@@ -654,7 +654,7 @@ class BurntToast
         # Declarations and Initializations
         # ----------------------------------------
         # Program's Logo Image
-        [string] $imageLogo = $NULL;
+        [string] $imageLogo = $GLOBAL:_PROGRAMDATA_ROAMING_PROJECT_ART_LOGO_PATH_;
 
         # Program's Banner Image
         [string] $imageBanner = $NULL;
@@ -662,8 +662,7 @@ class BurntToast
 
 
         # Determine what image will be shown to the user:
-        if ($graphicOption -eq [BurntToastShowGraphic]::Logo)   { $imageLogo    = $(GLOBAL:_PROGRAMDATA_ROAMING_PROJECT_ART_LOGO_PATH_);   }
-        else                                                    { $imageBanner  = $(GLOBAL:_PROGRAMDATA_ROAMING_PROJECT_ART_BANNER_PATH_); }
+        if ($graphicOption -eq [BurntToastShowGraphic]::Banner) { $imageBanner  = $GLOBAL:_PROGRAMDATA_ROAMING_PROJECT_ART_BANNER_PATH_; }
 
 
         # Show message
