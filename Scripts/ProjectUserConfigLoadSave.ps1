325803c8cbfe67cac68a7e618cd2b5865fec9a60
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2024-02-11 17:39:41 -0700

    [WIP] Added new function, Create() - NOT FINISHED
    This function, once completed, will create the User Configuration file within the specified directory.
    
    TODO: Need to create a function that can easily create files and can be used across the entire program without encouraging duplicated code segments.

--- Scripts/ProjectUserConfigLoadSave.ps1
+++ Scripts/ProjectUserConfigLoadSave.ps1
@@ -31,4 +31,118 @@
 
 class ProjectUserConfigurationLoadSave
 {
+   <# Create Project User Configuration
+    # -------------------------------
+    # Documentation:
+    #  This function will create a Project User Configuration file within the PowerShell Compact-Archive-
+    #   Tool's Project's directory.
+    # -------------------------------
+    # Input:
+    #  [ProjectUserConfiguration] User Configuration
+    #   The User Configuration contents that will be inserted into the User Config file.
+    #  [ProjectMetaData] Project's Meta Data
+    #   Desired Project's Meta Data information in which the User Config file will be created.
+    # -------------------------------
+    # Output:
+    #  $true    = Successfully created the User Config file.
+    #  $false   = Failed to create the User Config file.
+    # -------------------------------
+    #>
+    static [bool] Create([ProjectUserConfiguration] $userConfig, `      # User Config. Contents
+                            [ProjectMetaData] $metaData)                # Project to Create User Config.
+    {
+        # Declarations and Initializations
+        # ----------------------------------------
+        # This will contain the contents of the User Config. object into a string.
+        [string] $userConfigContentsString = $NULL;
+        # ----------------------------------------
+
+
+
+        # Make sure that the PSCAT Project Directory exists before continuing forward.
+        if (![CommonIO]::CheckPathExists($metaData.GetMetaFilePath(), $true))
+        {
+            # Project Directory does not exist, cannot create User Config. file.
+
+            # * * * * * * * * * * * * * * * * * * *
+            # Debugging
+            # --------------
+
+            # Generate the Message Box string that will be presented to the user
+            [string] $displayErrorMessage = ("The project named '$($metaData.GetProjectName())' does not " + `
+                                            "appear to be installed correctly!`r`n" + `
+                                            "Could not find installation path:`r`n" + `
+                                            $metaData.GetMetaFilePath());
+
+            # Generate the initial message
+            [string] $logMessage = "$($GLOBAL:_PROGRAMNAMESHORT_) Project directory does not exist, cannot create User Config file!"
+
+            # Generate any additional information that might be useful
+            [string] $logAdditionalMSG = ("$($GLOBAL:_PROGRAMNAMESHORT_) Project to Inspect:`r`n"   + `
+                                        "`tName:                " + $metaData.GetProjectName()              + "`r`n" + `
+                                        "`tCodename:            " + $metaData.GetProjectCodeName()          + "`r`n" + `
+                                        "`tRevision:            " + $metaData.GetProjectRevision()          + "`r`n" + `
+                                        "`tOutput Filename:     " + $metaData.GetProjectOutputFileName()    + "`r`n" + `
+                                        "`tWebsite URL:         " + $metaData.GetProjectURLWebsite()        + "`r`n" + `
+                                        "`tWiki URL:            " + $metaData.GetProjectURLWiki()           + "`r`n" + `
+                                        "`tSource URL:          " + $metaData.GetProjectURLSourceCode()     + "`r`n" + `
+                                        "`tSignature:           " + $metaData.GetMetaGUID()                 );
+
+            # Pass the information to the logging system
+            [Logging]::LogProgramActivity(  $logMessage, `              # Initial message
+                                            $logAdditionalMSG, `        # Additional information
+                                            [LogMessageLevel]::Error);  # Message level
+
+            # Display a message to the user that there was no projects found and log that same message for
+            #  referencing purpose.
+            [Logging]::DisplayMessage($logMessage, `                    # Message to display
+                                        [LogMessageLevel]::Error);      # Message level
+
+            # Alert the user through a message box as well that an issue had occurred;
+            #   the message will be brief as the full details remain within the terminal.
+            [CommonGUI]::MessageBox($displayErrorMessage, [System.Windows.MessageBoxImage]::Hand) | Out-Null;
+
+            # * * * * * * * * * * * * * * * * * * *
+
+
+            # Cannot create User Config file.
+            return $false;
+        } # if : PSCAT Proj. Dir. does not Exist
+
+
+        # Craft the string of all of the properties contained within the Project User Configuration object.
+        $userConfigContentsString = [ProjectUserConfigurationLoadSave]::CreateUserConfigurationString($userConfig);
+
+
+        # Create the User Configuration file
+        #TODO: Need to create a file, function needs to be created in CommonIO
+        #       MakeFile(PATH, WHAT)
+    } # Create()
+
+
+
+
+   <# Create User Configuration String
+    # -------------------------------
+    # Documentation:
+    #  This function will generate the output that will be inserted into the User Configuration file.
+    # -------------------------------
+    # Input:
+    #  [ProjectUserConfiguration] User Configuration
+    #   Containing the user's preferences that will be placed in the User Config. file.
+    # -------------------------------
+    # Output:
+    #  [string] User Configuration String
+    #   The User Configuration Preferences as a complete string that can be written to a file.
+    # -------------------------------
+    #>
+    static hidden [string] CreateUserConfigurationString([ProjectUserConfiguration] $userConfig)
+    {
+        return ("GameProjectSourcePath = " +  $userConfig.GetGameProjectSourcePath() + "`r`n");
+    } # CreateUserConfigurationString()
+
+
+
+    # Load Project User COnfiguration
+    # Update Project User Configuration
 } # ProjectUserConfigurationLoadSave
