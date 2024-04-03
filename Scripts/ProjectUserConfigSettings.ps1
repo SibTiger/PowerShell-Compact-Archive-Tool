cd9f1e2ceee4233203c160786ee3a2581c1096b1
Nicholas Gautier <Nicholas.Gautier@Outlook.com>
2024-04-02 18:34:18 -0600

    Added UpdateProjectSourcePath()

--- Scripts/ProjectUserConfigSettings.ps1
+++ Scripts/ProjectUserConfigSettings.ps1
@@ -32,4 +32,165 @@
 
 class ProjectUserConfigurationSettings
 {
+   <# Update Project Source Path
+    # -------------------------------
+    # Documentation:
+    #  This function will allow the user to specify where the project's source files are located within
+    #   their local system.
+    # -------------------------------
+    # Output:
+    #   $true   = Successfully updated the PSCAT Project Configuration File
+    #   $false  = Failed to update the PSCAT Project Configuration File.
+    # -------------------------------
+    #>
+    static [bool] UpdateProjectSourcePath()
+    {
+        # ----------------------------------------
+        # Grab the current instance of the Project Info.
+        [ProjectInformation] $projectInfo = [ProjectInformation]::GetInstance();
+
+        # The new path provided by the user.
+        [string] $newPath = $NULL;
+        # ----------------------------------------
+
+
+
+        # Clear the terminal of all previous text; keep the space clean so that it is easier
+        #   for the user to read the information presented.
+        [CommonIO]::ClearBuffer();
+
+
+        # Draw Program Information Header
+        [CommonCUI]::DrawProgramTitleHeader();
+
+
+        # Show the user that they are at the Update Project User Configuration Menu
+        [CommonCUI]::DrawSectionHeader("Update $($projectInfo.GetProjectName()) Source Path");
+
+
+        # Provide some whitespace padding
+        [Logging]::DisplayMessage("`r`n");
+
+
+        # Provide the instructions
+        [ProjectUserConfigurationSettings]::__DrawProjectSourcePathInstructions();
+
+
+        # Show the current path of the project's assets to the user, to help guide them with their selection.
+        [Logging]::DisplayMessage("Current Project Source Path Location: $($projectInfo.GetProjectPath())")
+
+
+        # Provide the Windows Directory Browser to the user
+        if (![CommonGUI]::BrowseDirectory("Select $($projectInfo.GetProjectPath()) Source Path", `
+                                        [BrowserInterfaceStyle]::Modern `,
+                                        [ref] $newPath))
+        {
+            # User canceled the operation.
+
+
+            # * * * * * * * * * * * * * * * * * * *
+            # Debugging
+            # --------------
+
+            # Generate the initial message
+            [string] $logMessage = ("User had cancelled the operation; no source path had been selected.");
+
+            # Generate any additional information that might be useful
+            [string] $logAdditionalMSG = "$($NULL)";
+
+            # Pass the information to the logging system
+            [Logging]::LogProgramActivity($logMessage, `                # Initial message
+                                        $logAdditionalMSG, `            # Additional information
+                                        [LogMessageLevel]::Verbose);    # Message level
+
+
+            # * * * * * * * * * * * * * * * * * * *
+
+
+            # Report that the user is cancelling the operation.
+            return $false;
+        } # if : User Canceled
+
+
+
+        # Update the path as requested by the user
+        if (!$projectInfo.SetProjectPath($newPath))
+        {
+            # Failed to update the Project's Source Path
+
+
+            # * * * * * * * * * * * * * * * * * * *
+            # Debugging
+            # --------------
+
+            # Generate the initial message
+            [string] $logMessage = ("Unable to update the project's source path.");
+
+            # Generate any additional information that might be useful
+            [string] $logAdditionalMSG = (  "Project Name: $($projectInfo.GetProjectName())`r`n"    + `
+                                            "`tRequested Path: $($newPath)`r`n"                     + `
+                                            "`tCurrent Path: $($project.GetProjectPath())");
+
+            # Pass the information to the logging system
+            [Logging]::LogProgramActivity($logMessage, `                # Initial message
+                                        $logAdditionalMSG, `            # Additional information
+                                        [LogMessageLevel]::Error);      # Message level
+
+
+            # * * * * * * * * * * * * * * * * * * *
+
+
+            # Failed to update
+            return $false;
+        } # if : Failed to update Project Source Path
+
+
+
+
+        # * * * * * * * * * * * * * * * * * * *
+        # Debugging
+        # --------------
+
+        # Generate the initial message
+        [string] $logMessage = ("Successfully updated the project's source path.");
+
+        # Generate any additional information that might be useful
+        [string] $logAdditionalMSG = (  "Project Name: $($projectInfo.GetProjectName())`r`n" + `
+                                        "`tSource Path: $($projectInfo.GetProjectPath())");
+
+        # Pass the information to the logging system
+        [Logging]::LogProgramActivity($logMessage, `                # Initial message
+                                    $logAdditionalMSG, `            # Additional information
+                                    [LogMessageLevel]::Verbose);    # Message level
+
+
+        # * * * * * * * * * * * * * * * * * * *
+
+
+        # Operation was successful
+        return $true;
+    } # UpdateProjectSourcePath()
+
+
+
+
+   <# Draw Project Source Path Instructions
+    # -------------------------------
+    # Documentation:
+    #  Provide the instructions to the user regarding the process in order to revise the path to the projects
+    #   source code and assets.
+    # -------------------------------
+    #>
+    hidden static [void] __DrawProjectSourcePathInstructions()
+    {
+        # Show the instructions to the user.
+        [Logging]::DisplayMessage( `
+            " To change the $($projectInfo.GetProjectName())'s source location, you will be using the Windows`r`n"          + `
+            "   Directory Browser to select a new path that contains the source material and assets for the`r`n"            + `
+            "   $($projectInfo.GetProjectName()) project.`r`n"                                                              + `
+            "`r`n"                                                                                                          + `
+            " You may cancel the operation by selecting 'Cancel' from the Directory Browser.`r`n"                           + `
+            "`r`n"                                                                                                          + `
+            "`r`n"                                                                                                          );
+    } # __DrawProjectSourcePathInstructions()
 } # ProjectUserConfigurationSettings
