8d6849e6b7c86b11963e51c2a52be27acfc71980
Nicholas Gautier <Gautier.Nicholas@Proton.me>
2024-11-08 20:06:13 -0700

    Removed [CommonFunctions]::IsAvailableZip()
    Removed the function, [CommonFunctions]::IsAvailableZip().
    
    #81

--- Scripts/CommonFunctions.ps1
+++ Scripts/CommonFunctions.ps1
@@ -524,36 +524,6 @@ class CommonFunctions
 
 
 
-   <# Is .NET Core ZIP Archive Available?
-    # -------------------------------
-    # Documentation:
-    #  This function will determine if the .NET Core Zip  functionality
-    #   is available on the host system.  In order for this operation
-    #   to work, we will use the Default Zip object to check if such
-    #   feature is present.
-    # -------------------------------
-    # Output:
-    #  [bool] .NET Core Zip Availability
-    #   When true, this will mean that Zip is available and can be used.
-    #   False, however, will mean that the Zip functionality is not available.
-    # -------------------------------
-    #>
-    static [bool] IsAvailableZip()
-    {
-        # Declarations and Initializations
-        # ----------------------------------------
-        # Latch onto the single instance of the Zip object
-        [DefaultCompress] $defaultCompress = [DefaultCompress]::GetInstance();
-        # ----------------------------------------
-
-
-        # Return the results from the detection function
-        return $defaultCompress.DetectCompressModule();
-    } # IsAvailableZip()
-
-
-
-
    <# Is String Empty
     # -------------------------------
     # Documentation:
