daa7768572859f94f46f0de5b0466a93d212eec5
Nicholas Gautier <Gautier.Nicholas@Proton.me>
2024-12-22 20:05:16 -0700

    Removed Verify from Loading UserConfig
    #109

--- Scripts/LoadSaveUserConfigs.ps1
+++ Scripts/LoadSaveUserConfigs.ps1
@@ -928,30 +928,6 @@ class LoadSaveUserConfiguration
 
 
 
-        # POWERSHELL'S ARCHIVE SETTINGS -- VERIFY BUILD
-        # - - - - - - - - - - - - - - - - - - - - - - -
-        try
-        {
-            # Set: Verify Build
-            $psArchive.SetVerifyBuild([bool]$cachedUserConfig[3].__verifyBuild);
-        } # Try : Load Value from Config
-
-        # Error trying to load variable into the current program's instance.
-        catch
-        {
-            # Because the value was unknown, we will keep what value is already stored.
-
-
-            # Provide error information to the user and logfile.
-            $this.__LoadStepWiseError("__verifyBuild", `                                # The Variable Name
-                                    "Archive Settings", `                               # The Variable Category
-                                    [string]$cachedUserConfig[3].__verifyBuild, `       # Value Stored in Config
-                                    [string]$psArchive.GetVerifyBuild(), `              # Current Value
-                                    $_.Exception);                                      # Exception Details
-        } # Catch : Unknown Value from Config.
-
-
-
         # Everything was okay, return a successful status
         return $true;
     } # __LoadStepWise()
