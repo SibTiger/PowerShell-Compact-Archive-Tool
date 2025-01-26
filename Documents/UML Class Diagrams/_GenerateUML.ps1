56e6f13a45da6148ab76e46973eba1baad374828
Nicholas Gautier <Gautier.Nicholas@Proton.me>
2025-01-26 00:54:17 -0700

    Updated Generate UML Script Instructions
    Provided instructions to install the required dependencies in order for the Write-CUClassDiagram to work.
    
    #136

--- Documents/UML Class Diagrams/_GenerateUML.ps1
+++ Documents/UML Class Diagrams/_GenerateUML.ps1
@@ -1,3 +1,15 @@
+# -------------------------------------
+# New Installation Instructions:
+#   - Install Chocolatey
+#       - [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#   - Find-module PSClassUtils | install-Module
+#       - https://github.com/Stephanevg/PSClassUtils
+#   - Install-CUDiagramPrerequisites
+#   - choco install graphviz
+#       - https://github.com/PrateekKumarSingh/AzViz/issues/12#issuecomment-816745045
+# -------------------------------------
+
+
 [System.Collections.ArrayList] $path = [System.Collections.ArrayList]@();
 
 
